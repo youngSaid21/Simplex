869fca15848dd961c8020fe35136124c7157fe25
saidoujalloh271@gmail.com <saidoujalloh271@gmail.com>
2024-05-13 22:29:49 +0100

    Logic correction

--- simplexapp/views.py
+++ simplexapp/views.py
@@ -1,178 +1,200 @@
 from django.shortcuts import render
+import heapq
 
-import numpy as np
+def slack_variables(c,A):
+    n = len(A)
+    c += [0]*n
+    
+    M = [[0]*n for j in range(n)]
+    
+    for i in range(len(M)):
+        M[i][i] = 1
+        A[i] += M[i]
 
-# Fonction pour ajouter des variables d'ecart
-def ajout_variables_ecart(nb_contraintes, objectif, contraintes):
-    matrice_identite = np.eye(nb_contraintes)
-    
-    matrice_null =  np.zeros((1, nb_contraintes))  # Assurez-vous que matrice_null est également 2D   
-    objectif = np.hstack((objectif, matrice_null))
-    contraintes = np.hstack((contraintes, matrice_identite))
-    return (objectif, contraintes)
-    
-# Retourner la solution realisable
-def solution_realisable(second_membre):
-    indices_tries = np.argsort(second_membre[:, 0])
-    matrice_triee = second_membre[indices_tries]
-    #print(matrice_triee)
-    legende = []
-    solution = []
-    #print(second_membre)
-    for i in range(1,len(matrice_triee)):
-        legende.append("x"+str(int(matrice_triee[i][0])))
-        solution.append(matrice_triee[i][1])
-        
-    #print(matrice_triee)
-    return [legende, solution, ['Z',matrice_triee[0][1]]]
 
-# Retourner l'indice de la variable entrante
-def indice_variable_entrante(objectif):
-     
-    # Utilisez un masque pour sélectionner uniquement les valeurs négatives
-    #valeurs_negatives = objectif[objectif < 0]
-   
-    # Récupérez l'indice de la valeur négative la plus petite
-         
-    return np.argmin(objectif)
+'''
+   Renvoie une matrice d'identité rectangulaire avec les entrées diagonales spécifiées, commençant éventuellement au milieu.
+'''
+def identity(numRows, numCols, val=1, rowStart=0):
+   return [[(val if i == j else 0) for j in range(numCols)]
+               for i in range(rowStart, numRows)]
 
-# Retourner l'indice de la variable sortante    
-def indice_variable_sortante(contraintes_etendus, second_membre, indice, nb_variable, nb_contraintes):
-    l = []
-    
-    #for i in range(nb_variable,len(second_membre)-1):
-    for i in range(nb_variable,nb_variable + nb_contraintes):
-        
-        
-        
-        #if second_membre[i][1] != 0:
-            
-        if contraintes_etendus[i-nb_variable][indice] != 0:
-            l.append([second_membre[i][0],second_membre[i][1]/contraintes_etendus[i-nb_variable][indice]])
-    
-    k = l[0]
-    
-    for i in range(len(l)):
-        if l[i][1] < k[1]:
-            k = l[i]
-                
-    return k[0]
- 
-# Diviser la ligne de pivot par le pivot
-def dif_ligne_piv(contraintes_etendus, second_membre, indice_entrante, indice_sortante, nb_variable):
-    
-    
-    #print((indice_sortante - 3))
-    
-    pivot = contraintes_etendus[indice_sortante - nb_variable - 1][indice_entrante] 
-    #print(pivot)
-    for j in range(len(contraintes_etendus[indice_sortante - nb_variable - 1])):
-        contraintes_etendus[indice_sortante - nb_variable - 1][j] = contraintes_etendus[indice_sortante - nb_variable - 1][j]/pivot
-    
-    #print("1",second_membre)
 
-    #print(second_membre[indice_sortante - 1][1]/pivot)
-    
-    second_membre[indice_sortante - 1][1] =  second_membre[indice_sortante - 1][1]/pivot 
-    
-    #print(second_membre[indice_sortante - 1][1])
-    
-    #print("2",second_membre)
-        
-# La variable entrante prend la place de la variable sortante    
-def echanger_entrante_sortante(second_membre, indice_entrante, indice_sortante):
-    i = np.where(second_membre[:, 0] == indice_entrante)[0][0]
-    j = np.where(second_membre[:, 0] == indice_sortante)[0][0]
-    
-    
-    second_membre[j][0] , second_membre[i][0] = second_membre[i][0],  second_membre[j][0]
-    
-# Pivotage                 
-def pivotage(second_membre, objectif_etendus, contraintes_etendus, indice_entrante, indice_sortante, nb_variable):
-    ligne_pivot = contraintes_etendus[indice_sortante - nb_variable - 1]
-    
-    pivot = objectif_etendus[0][indice_entrante]
-      
-    for i in range(len(objectif_etendus[0])):
-        objectif_etendus[0][i] = objectif_etendus[0][i] - pivot * ligne_pivot[i]
-        
-    
-    #print(" Z ",second_membre[-1][1])
-    #print("Pivot : ", pivot)
-    #print(second_membre)
-    #print("Valeur sortante : ",second_membre[indice_sortante - 1])
-    
-    #print(indice_sortante - 1)
-        
-    second_membre[-1][1] = second_membre[-1][1] - pivot * second_membre[indice_sortante - 1][1]
-    
-    
-    
-    for i in range(nb_variable,len(second_membre)-1):
-        if i != indice_sortante - 1:
-            pivot = contraintes_etendus[i-nb_variable][indice_entrante]
-            #print("pivot : ",pivot)
-    
-            second_membre[i][1] = second_membre[i][1] - pivot * second_membre[indice_sortante-1][1]
-            
-    #print(second_membre)
-            
-            
-    
-    for i in range(len(contraintes_etendus)):
-        if i != indice_sortante - nb_variable - 1:
-            pivot = contraintes_etendus[i][indice_entrante]
-            for j in range(len(contraintes_etendus[i])):
-                contraintes_etendus[i][j] = contraintes_etendus[i][j] - pivot * ligne_pivot[j]
-    
-       
-    
-def solution_simplexe(nb_variable, nb_contraintes, objectif, contraintes, second_membre):
-    # Ajouter des variables d'ecart
-    objectif_etendus = ajout_variables_ecart(nb_contraintes, objectif, contraintes)[0]
-    contraintes_etendus = ajout_variables_ecart(nb_contraintes, objectif, contraintes)[1]
-    
-    
-    while 1:
-        
-        
-        
-        # Indice de la variable entrante
-        indice_entrante = int(indice_variable_entrante(objectif_etendus))
-        #print(indice_entrante)
-        #print(objectif_etendus[0][indice_entrante])
-        
-        if objectif_etendus[0][indice_entrante] >= 0:
-            solution = solution_realisable(second_membre)
-            
-            return solution
-            
-            
+'''
+   standardForm: [float], [[float]], [float], [[float]], [float], [[float]], [float] -> [float], [[float]], [float]
+    Convertir un programme linéaire sous forme générale en forme standard pour le
+    algorithme simplex. Les entrées sont supposées avoir les bonnes dimensions: coût
+    est une liste de longueur n, GreaterThans est une matrice n par m, gtThreshold est un vecteur
+    de longueur m, avec le même motif pour les entrées restantes. Non
+    des erreurs de dimension sont détectées et nous supposons qu'il n'y a pas de variables sans restriction.
+'''
+def standardForm(cost, greaterThans=[], gtThreshold=[], lessThans=[], ltThreshold=[],
+                equalities=[], eqThreshold=[], maximization=True):
+   newVars = 0
+   numRows = 0
+   if gtThreshold != []:
+      newVars += len(gtThreshold)
+      numRows += len(gtThreshold)
+   if ltThreshold != []:
+      newVars += len(ltThreshold)
+      numRows += len(ltThreshold)
+   if eqThreshold != []:
+      numRows += len(eqThreshold)
 
-        indice_sortante = int(indice_variable_sortante(contraintes_etendus, second_membre, indice_entrante, nb_variable, nb_contraintes))
-        
-       
-        
-        dif_ligne_piv(contraintes_etendus, second_membre, indice_entrante, indice_sortante, nb_variable)
-        
-        
-        
-        echanger_entrante_sortante(second_membre, indice_entrante, indice_sortante)
-           
-        pivotage(second_membre, objectif_etendus, contraintes_etendus, indice_entrante, indice_sortante, nb_variable)
-        
+   if not maximization:
+      cost = [-x for x in cost]
+
+   if newVars == 0:
+      return cost, equalities, eqThreshold
+
+   newCost = list(cost) + [0] * newVars
+
+   constraints = []
+   threshold = []
+
+   oldConstraints = [(greaterThans, gtThreshold, -1), (lessThans, ltThreshold, 1),
+                     (equalities, eqThreshold, 0)]
+
+   offset = 0
+   for constraintList, oldThreshold, coefficient in oldConstraints:
+      constraints += [c + r for c, r in zip(constraintList,
+         identity(numRows, newVars, coefficient, offset))]
+
+      threshold += oldThreshold
+      offset += len(oldThreshold)
+
+   return newCost, constraints, threshold
+
+
+def dot(a,b):
+   return sum(x*y for x,y in zip(a,b))
+
+def column(A, j):
+   return [row[j] for row in A]
+
+def transpose(A):
+   return [column(A, j) for j in range(len(A[0]))]
+
+def isPivotCol(col):
+   return (len([c for c in col if c == 0]) == len(col) - 1) and sum(col) == 1
+
+def variableValueForPivotColumn(tableau, column):
+   pivotRow = [i for (i, x) in enumerate(column) if x == 1][0]
+   return tableau[pivotRow][-1]
+
+# assume the last m columns of A are the slack variables; the initial basis is
+# the set of slack variables
+def initialTableau(c, A, b):
+   tableau = [row[:] + [x] for row, x in zip(A, b)]
+   tableau.append([ci for ci in c] + [0])
+   return tableau
+
+
+def primalSolution(tableau):
+   # the pivot columns denote which variables are used
+   columns = transpose(tableau)
+   indices = [j for j, col in enumerate(columns[:-1]) if isPivotCol(col)]
+   return [(colIndex, variableValueForPivotColumn(tableau, columns[colIndex]))
+            for colIndex in indices]
+
+
+def objectiveValue(tableau):
+   return -(tableau[-1][-1])
+
+
+def canImprove(tableau):
+   lastRow = tableau[-1]
+   return any(x > 0 for x in lastRow[:-1])
+
+
+# this can be slightly faster
+def moreThanOneMin(L):
+   if len(L) <= 1:
+      return False
+
+   x,y = heapq.nsmallest(2, L, key=lambda x: x[1])
+   return x == y
+
+
+def findPivotIndex(tableau):
+   # pick minimum positive index of the last row
+   column_choices = [(i,x) for (i,x) in enumerate(tableau[-1][:-1]) if x > 0]
+   column = min(column_choices, key=lambda a: a[1])[0]
+
+   # check if unbounded
+   if all(row[column] <= 0 for row in tableau):
+      raise Exception('Linear program is unbounded.')
+
+   # check for degeneracy: more than one minimizer of the quotient
+   quotients = [(i, r[-1] / r[column])
+      for i,r in enumerate(tableau[:-1]) if r[column] > 0]
+
+   if moreThanOneMin(quotients):
+      raise Exception('Linear program is degenerate.')
+
+   # pick row index minimizing the quotient
+   row = min(quotients, key=lambda x: x[1])[0]
+
+   return row, column
+
+
+def pivotAbout(tableau, pivot):
+   i,j = pivot
+
+   pivotDenom = tableau[i][j]
+   tableau[i] = [x / pivotDenom for x in tableau[i]]
+
+   for k,row in enumerate(tableau):
+      if k != i:
+         pivotRowMultiple = [y * tableau[k][j] for y in tableau[i]]
+         tableau[k] = [x - y for x,y in zip(tableau[k], pivotRowMultiple)]
+
+
+'''
+   simplex: [float], [[float]], [float] -> [float], float
+    Résolvez le programme linéaire de forme standard donné:
+       max <c, x>
+       s.t. Ax = b
+            x> = 0
+    fournissant la solution optimale x * et la valeur de la fonction objectif
+'''
+def solution_variable(nb_variable, liste_tuples):
+    v = [(i,0) for i in range(nb_variable)]
+    
+    for i in range(nb_variable):
+        for j in range(nb_variable):
+            if liste_tuples[j][0] == v[i][0]:
+                v[i] = liste_tuples[j]
+                break
+    return v
+
+def simplex(c, A, b):
+   nb_variable = len(c)
+   slack_variables(c,A)
+   tableau = initialTableau(c, A, b)
+   #print("Initial tableau:")
+   #for row in tableau:
+      #print(row)
+   #print()
+
+   while canImprove(tableau):
+      pivot = findPivotIndex(tableau)
+      #print("Next pivot index is=%d,%d \n" % pivot)
+      pivotAbout(tableau, pivot)
+      #print("Tableau after pivot:")
+      '''for row in tableau:
+         print(row)
+      print()'''
+    
+   primaSolution = primalSolution(tableau)
+   print(primaSolution)
+
+   return tableau, solution_variable(nb_variable, primaSolution), objectiveValue(tableau)
+   #return tableau, primalSolution(tableau), objectiveValue(tableau) 
+
+
 def index(request):
-    # Exemple d'utilisationnb_variable = 2
-    nb_variable = 2
-    nb_contraintes = 3
-    objectif = np.array([[1,-4, -7]])
-    contraintes = np.array([[0, 4, 0],
-                            [0, 0, 5],
-                            [0, 4, 7]])
-    second_membre = np.array([[1,0], [2,0], [3,13], [4,6], [5,5], [0,0]], dtype=float)
-
-    solution = solution_simplexe(nb_variable, nb_contraintes, objectif, contraintes, second_membre)
-    return render(request, 'index.html', {'solution': solution})
+    return render(request, 'index.html', {})
 
 def saisie(request):
     if request.method == 'POST':
@@ -214,59 +236,39 @@ def result(request, nb_variable, nb_contraintes):
         #nb_contraintes = 
 
         # Objectifs
-        l = [1]
+        c = []
         for i in range(nb_variable):
             number = 'number' + str(i+1)
-            l.append(-int(request.POST.get(number)))
-
-        objectif = np.array([l])
-
-        print("Objectif : ",objectif)
+            c.append(int(request.POST.get(number)))
 
         # Contraintes
-        l = []
+        A = []
         for i in range(nb_contraintes):
-            tmp = [0]
+            tmp = []
             for j in range(nb_variable):
                 number = 'contrainte' + str(i) + str(j)
                 tmp.append(int(request.POST.get(number)))
-            l.append(tmp)
-
-        contraintes = np.array(l)
-
-        #print("Les contraintes : ")
-        print(contraintes)
+            A.append(tmp)
 
         # Second Membre
-        l = []
-        for i in range(nb_variable):
-            l.append([i+1, 0])
-
+        b = []
+        
         k = 1
         for j in range(nb_variable+1, nb_variable + nb_contraintes + 1):
             number = 'second_membre' + str(k)
-            l.append([j,int(request.POST.get(number))])
+            b.append(int(request.POST.get(number)))
             k += 1
-        l.append([0,0])
-
-        second_membre = np.array(l, dtype=float)
-
-        print(second_membre)
-
-        solution = solution_simplexe(nb_variable, nb_contraintes, objectif, contraintes, second_membre)
-        z = solution[2][1]
-        print(solution)
+        
+        t, s, z = simplex(c, A, b)
+        print(s)
         print(z)
         dictio = {}
         for i in range(nb_variable):
-            dictio[solution[0][i]] = solution[1][i]
-
-        print(dictio)
+            dictio[s[i][0] + 1] = s[i][1]
 
         context = {
             'dictio': dictio,
             'z': z,
         }
         
-
     return render(request, 'result.html', context)
